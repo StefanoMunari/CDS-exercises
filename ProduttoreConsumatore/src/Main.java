340c7ebe107ab6b368a61c4fefb44cfad51093de
Stefano Munari <stefano.munari@hotmail.it>
2015-11-20 20:28:51 +0100

    aggiunta javadoc, visibilità package

--- ProduttoreConsumatore/src/Main.java
+++ ProduttoreConsumatore/src/Main.java
@@ -1,15 +1,23 @@
+/**
+ * @author Stefano Munari <stefanomunari.sm@gmail.com>
+ *
+ * @class producerConsumer.Main
+ * @classdesc Entry point of the program, instantiates the monitor and the
+ * threads related to the Producer objects and the Consumer objects.
+ */
 package producerConsumer;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) {
 		Monitor m= new Monitor();
 
-		// VERSIONE CON 1 PRODUTTORE ED 1 CONSUMATORE
+// VERSION WITH 1 PRODUCER AND 1 CONSUMER
+/*
 		new Thread(new Consumer(m)).start();
 		new Thread(new Producer(m)).start();
-
-		// VERSIONE CON N PRODUTTORI E CONSUMATORI
+*/
+// VERSION WITH N PRODUCERS AND N CONSUMERS
 		final int n= 9;
 		for(int i=0; i< n; i++)
 		{
