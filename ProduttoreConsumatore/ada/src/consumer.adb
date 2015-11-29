13a29dd29bdbef52d3f2caebc25dc341de687c23
snate <sebastiano.valle@studenti.unipd.it>
2015-11-29 20:12:59 +0100

    AdaPC: Add consumer task type
    Consumer takes the resources that are put in a buffer.

---
+++ ProduttoreConsumatore/ada/src/consumer.adb
@@ -0,0 +1,21 @@
+with System;
+with Ada.Text_IO;
+
+package body Consumer is
+
+   task body Consumer_T is
+
+      use Ada.Text_IO,
+          Buffers;
+
+      C_Interval : constant Duration := 1.0;
+
+   begin
+      loop
+         Buffer.Take;
+         Put_Line("Take");
+         delay C_Interval;
+      end loop;
+   end Consumer_T;
+
+end Consumer;
---
+++ ProduttoreConsumatore/ada/src/consumer.ads
@@ -0,0 +1,10 @@
+with Buffers;
+
+package Consumer is
+
+   task type Consumer_T is
+   end Consumer_T;
+   type Consumer_Ref is access Consumer_T;
+
+end Consumer;
+
