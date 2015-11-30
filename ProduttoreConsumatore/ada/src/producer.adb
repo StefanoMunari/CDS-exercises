82959f66caea7551e91d0a4a4ad8037577a73a58
snate <sebastiano.valle@studenti.unipd.it>
2015-11-30 22:37:44 +0100

    AdaPC: Add producer documentation

--- ProduttoreConsumatore/ada/src/producer.adb
+++ ProduttoreConsumatore/ada/src/producer.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO;
 with Buffer;
 with Random;
- 
+
 package body Producer is
 
    task body Producer_T is
@@ -11,8 +11,11 @@ package body Producer is
           Random;
    begin
       loop
+         --  producer puts an item in the buffer
          Buffer_I.Put;
          Put_Line("working Producer Id:" & Integer'Image(Id));
+         --  producer waits for a random interval of time before generating
+         --  another item
          delay Duration(generateRandom(1,4));
       end loop;
    end Producer_T;
--- ProduttoreConsumatore/ada/src/producer.ads
+++ ProduttoreConsumatore/ada/src/producer.ads
@@ -1,5 +1,8 @@
 package Producer is
- 
+
+   --  producer is not an anonymous task but a task type: it can be instantiated
+   --  more than once because the system allows the presence of multiple
+   --  producers that generates items and put them concurrently in the buffer
    task type Producer_T (Id : integer) is
    end Producer_T;
    type Producer_Ref is access Producer_T;
