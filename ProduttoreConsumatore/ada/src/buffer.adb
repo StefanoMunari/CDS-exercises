fc72c387d5a3112754e51e8ab201e49f90eda18f
snate <sebastiano.valle@studenti.unipd.it>
2015-11-30 22:13:40 +0100

    AdaPC: Add buffer documentation

--- ProduttoreConsumatore/ada/src/buffer.adb
+++ ProduttoreConsumatore/ada/src/buffer.adb
@@ -1,6 +1,6 @@
 with System;
 with Ada.Text_IO;
- 
+
 package body Buffer is
 
    use Ada.Text_IO;
@@ -9,6 +9,9 @@ package body Buffer is
 
       entry Put when Number_Of_Items < Buffer_Size is
       begin
+         --  the number of items increases without the possibility to exceed
+         --  buffer size, since this entry is available only when the buffer is
+         --  not full
          Number_Of_Items := Number_Of_Items + 1;
          Put_Line("Put an object => current buffer status: "
                   & Positive'Image(Number_Of_Items));
@@ -16,6 +19,9 @@ package body Buffer is
 
       entry Take when Number_Of_Items > 0 is
       begin
+         --  the number of items decreases without the possibility to raise an
+         --  exception, since this entry is available only when the buffer is
+         --  not empty
          Number_Of_Items := Number_Of_Items - 1;
          Put_Line("Take an object =>  current buffer status: "
                   & Positive'Image(Number_Of_Items));
--- ProduttoreConsumatore/ada/src/buffer.ads
+++ ProduttoreConsumatore/ada/src/buffer.ads
@@ -1,10 +1,17 @@
 package Buffer is
- 
+
+   --  size of the buffer, i.e. the number of items it can store
    Buffer_Size : constant Natural := 4;
+   --  define a type to be sure that items number does not exceed buffer size
    subtype Max_In_Buffer is Natural range 0 .. Buffer_Size;
 
+   --  the buffer is an anonymous protected resource, since this version of the
+   --  program does not take in account the possibility to have a distributed
+   --  system of several buffers
    protected Buffer_I is
+      --  buffer provides an entry to allow producers to put an item in
       entry Put;
+      --  buffer provides an entry to allow consumers to take an item from
       entry Take;
    private
       Number_Of_Items : Max_In_Buffer := 0;
