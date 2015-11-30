2c075fd13545ce735dd14d953cc970e1999597f8
snate <sebastiano.valle@studenti.unipd.it>
2015-11-30 22:33:42 +0100

    AdaPC: Add consumer documentation

--- ProduttoreConsumatore/ada/src/consumer.adb
+++ ProduttoreConsumatore/ada/src/consumer.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO;
 with Buffer;
 with Random;
- 
+
 package body Consumer is
 
    task body Consumer_T is
@@ -12,8 +12,11 @@ package body Consumer is
 
    begin
       loop
+         --  consumer takes (consumes) an item from the buffer
          Buffer_I.Take;
          Put_Line("working Consumer Id: " & Integer'Image(Id) );
+         --  consumer waits for a random interval of time before consuming
+         --  another item
          delay Duration(generateRandom(1,3));
       end loop;
    end Consumer_T;
--- ProduttoreConsumatore/ada/src/consumer.ads
+++ ProduttoreConsumatore/ada/src/consumer.ads
@@ -1,5 +1,8 @@
 package Consumer is
- 
+
+   --  consumer is not an anonymous task: it can be instantiated more than once
+   --  because the system allows the presence of multiple consumers that access
+   --  the buffer concurrently
    task type Consumer_T (Id : integer) is
    end Consumer_T;
    type Consumer_Ref is access Consumer_T;
