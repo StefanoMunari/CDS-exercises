a46a736fc2de3b2f5c453170b37a692c2c80992f
snate <sebastiano.valle@studenti.unipd.it>
2015-11-30 22:57:35 +0100

    AdaPC: Add random function documentation

--- ProduttoreConsumatore/ada/src/random.adb
+++ ProduttoreConsumatore/ada/src/random.adb
@@ -1,14 +1,16 @@
 with Ada.Numerics.discrete_Random;
 with Ada.Text_IO;
- 
+
 package body Random is
 
    function generateRandom (LowerBound, UpperBound : in Positive) return Positive is
 
       use Ada.Text_IO;
 
+      --  define an exception type to handle faulty parameters passing
       Bound_Error : exception;
 
+      --  function that checks if user gave proper (valid) bounds
       function validBounds (LowerBound, UpperBound : in Positive)
                              return Boolean is
       begin
@@ -25,16 +27,19 @@ package body Random is
       Num : Rand_Range;
 
    begin
+      --  check if bounds are valid
       if validBounds(LowerBound,UpperBound) then
+         --  generates and returns a new random number between the given bounds
          Rand_Int.Reset(seed);
          Num := Rand_Int.Random(seed);
          return Positive'Value(Rand_Range'Image(Num));
       else
          raise Bound_Error;
       end if;
-      exception
-         when Bound_Error =>
-             Put_Line(Standard_Error,"ERROR: range's limits are not valid!!!");
+   exception
+      --  causes invoker termination with Program Error
+      when Bound_Error =>
+         Put_Line(Standard_Error,"ERROR: range's limits are not valid!!!");
    end generateRandom;
 
 end Random;
--- ProduttoreConsumatore/ada/src/random.ads
+++ ProduttoreConsumatore/ada/src/random.ads
@@ -1,4 +1,6 @@
 package Random is
+
+   --  function that generates random numbers between a lower and an upper bound
    function generateRandom (LowerBound, UpperBound : in Positive) return Positive;
 end Random;
- 
+
