e0d4f60916e854efcd1af99fa3e05d5d219e27bd
snate <sebastiano.valle@studenti.unipd.it>
2015-12-08 12:11:05 +0100

    CS: Give Get and Put entries to a Table

--- CigaretteSmokers/ada/src/table.ads
+++ CigaretteSmokers/ada/src/table.ads
@@ -1,12 +1,16 @@
 package Table is
+      --  TODO: Set proper priority to Table
 
    -- place where smokers receive goods from pushers
    protected Status is
-      entry Get;
-      entry Put;
+      --  calling this entry, smokers tells what they have and Table have to
+      --+ provide them what they need
+      entry Get(Owned_Res : Integer);
+      --  calling this entry, pushers tells what they *don't* put on the table
+      entry Put(Missing_Res : Integer);
    end Status;
 
-   procedure Get renames Status.Get;
-   procedure Put renames Status.Put;
+   procedure Get(Owned_S : Integer) renames Status.Get;
+   procedure Put(Missing_Res : Integer) renames Status.Put;
 
 end Table;
